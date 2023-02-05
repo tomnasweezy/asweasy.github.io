8fae7c297aa6b93bab0ce88d9e57e9c1e7e3b535
tomnasweezy <bosstomna@gmail.com>
2023-02-05 21:38:23 +0100

    mobile view + additonal css changes

--- src/App.js
+++ src/App.js
@@ -58,7 +58,8 @@ function App() {
         {!biog && (
           <div id="biowraper" className="biowraper">
             <p id="bio" className="bio">
-              Hi, I'm Ahmed a
+              Hi, I'm Ahmed<br></br>
+              <span className="ltbca">like to be called an</span>
             </p>
             <div className="morphing">
               <p className="POS">Implementation Manager</p>
@@ -77,18 +78,43 @@ function App() {
             </div>
             <div className="infowrapper">
               <div>
+                {/* https://reactsvgicons.com/search?q=linkedIn */}
                 <a href="https://github.com/tomnasweezy">
-                  <img src={github} />
+                  <svg
+                    viewBox="0 0 1024 1024"
+                    fill="black"
+                    height="1em"
+                    width="1em"
+                  >
+                    <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0138.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z" />
+                  </svg>
                 </a>
               </div>
               <div>
                 <a href="https://www.linkedin.com/in/ahmed-hisham-431069120/">
-                  <img src={linkedin} />
+                  <svg
+                    viewBox="0 0 1024 1024"
+                    fill="black"
+                    height="1em"
+                    width="1em"
+                  >
+                    <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM349.3 793.7H230.6V411.9h118.7v381.8zm-59.3-434a68.8 68.8 0 1168.8-68.8c-.1 38-30.9 68.8-68.8 68.8zm503.7 434H675.1V608c0-44.3-.8-101.2-61.7-101.2-61.7 0-71.2 48.2-71.2 98v188.9H423.7V411.9h113.8v52.2h1.6c15.8-30 54.5-61.7 112.3-61.7 120.2 0 142.3 79.1 142.3 181.9v209.4z" />
+                  </svg>
                 </a>
               </div>
+
               <div>
                 <a href="https://drive.google.com/file/d/1NALHNTPv1JlSY7ikJKWipbKl9Rhg9XfH/view?usp=sharing">
-                  <img className="resume" src={resume} />
+                  {/* <img className="resume" src={resume} /> */}
+                  <svg
+                    viewBox="0 0 512 512"
+                    fill="black"
+                    height="1em"
+                    width="1em"
+                  >
+                    <path d="M428 224H288a48 48 0 01-48-48V36a4 4 0 00-4-4h-92a64 64 0 00-64 64v320a64 64 0 0064 64h224a64 64 0 0064-64V228a4 4 0 00-4-4zm-92 160H176a16 16 0 010-32h160a16 16 0 010 32zm0-80H176a16 16 0 010-32h160a16 16 0 010 32z" />
+                    <path d="M419.22 188.59L275.41 44.78a2 2 0 00-3.41 1.41V176a16 16 0 0016 16h129.81a2 2 0 001.41-3.41z" />
+                  </svg>
                 </a>
               </div>
             </div>
